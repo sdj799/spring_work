e3edf1261d3ca66260a8ee63e4246bdb5a3536b7
dongjun <sdj799@naver.com>
2023-05-15 14:48:27 +0900

    05.15. commit

--- SpringDBAccess/src/main/java/com/spring/db/service/BoardService.java
+++ SpringDBAccess/src/main/java/com/spring/db/service/BoardService.java
@@ -6,37 +6,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.spring.db.model.BoardVO;
-import com.spring.db.repository.IBoardDAO;
+import com.spring.db.repository.IBoardMapper;
 
 @Service
 public class BoardService implements IBoardService {
 
+//	@Autowired
+//	@Qualifier("boardDAO")
+//	private IBoardDAO dao;
+	
 	@Autowired
-	private IBoardDAO dao;
+	private IBoardMapper mapper;
 	
 	@Override
 	public void insertArticle(BoardVO vo) {
-		dao.insertArticle(vo);
+		mapper.insertArticle(vo);
 	}
 
 	@Override
 	public List<BoardVO> getArticles() {
-		return dao.getArticles();
+		return mapper.getArticles();
 	}
 
 	@Override
 	public BoardVO getArticle(int bno) {
-		return dao.getArticle(bno);
+		return mapper.getArticle(bno);
 	}
 
 	@Override
 	public void deleteArticle(int bno) {
-		dao.deleteArticle(bno);
+		mapper.deleteArticle(bno);
 	}
 
 	@Override
 	public void updateArticle(BoardVO vo) {
-		dao.updateArticle(vo);
+		mapper.updateArticle(vo);
 	}
 
 }
